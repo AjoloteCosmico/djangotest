7964386b3eeb3d8e9bb7b580a2a8a7facb22b42c
Felipe Iván González Lucas <fgonzalez@pve.unam.mx>
2022-09-14 13:01:16 -0500

    first commit

---
+++ polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+     #ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
+    ]
