a1e878e10c4ef523268074680abb9a00606a97c7
Felipe Iván González Lucas <fgonzalez@pve.unam.mx>
2022-09-14 13:44:42 -0500

    añadí respuestas

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
-
+#Modelos de Prueba los vi en un tutorial xd
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -9,13 +9,21 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
        return self.choice_text
-    
+
+#Creando los modelos que si usaremos (preeliminares)
+class respuestas(models.Model):
+    #Datos personales
+    cuenta=models.CharField(max_length=9)
+    nombre=models.CharField(max_length=50)
+    materno=models.CharField(max_length=50)
+    paterno=models.CharField(max_length=50)
+    #primer reactivo de la encuesta
+    trabaja=models.BooleanField()
+
 
